2bca6beed63e624a53b47499ee72076f38f07372
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-03-18 10:57:34 +0100

    frends

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,75 @@
+import css from "./Profile.module.css";
+import PropTypes from "prop-types";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  const { followers, views, likes } = stats;
+
+  return (
+    <div className={css.container}>
+      <div>
+        <img className={css.img} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.listItem}>
+          <span className={css.listTitle}>Followers</span>
+          <span className={css.listAmount}>{followers}</span>
+        </li>
+        <li className={css.listItem}>
+          <span className={css.listTitle}>Views</span>
+          <span className={css.listAmount}>{views}</span>
+        </li>
+        <li className={css.listItem}>
+          <span className={css.listTitle}>Likes</span>
+          <span className={css.listAmount}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  image: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
+
+export default Profile;
+
+{
+  /* <div>
+  <div>
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="User avatar"
+    />
+    <p>Petra Marica</p>
+    <p>@pmarica</p>
+    <p>Salvador, Brasil</p>
+  </div>
+
+  <ul>
+    <li>
+      <span>Followers</span>
+      <span>1000</span>
+    </li>
+    <li>
+      <span>Views</span>
+      <span>2000</span>
+    </li>
+    <li>
+      <span>Likes</span>
+      <span>3000</span>
+    </li>
+  </ul>
+</div>; */
+}
